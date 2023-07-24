bf16f81d18101197754f36b6bbfa65e2cba5daac
Cohen Erickson <cohenerickson@gmail.com>
2023-07-23 21:07:45 -0500

    fix export

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "[name].js",
-    library: "Filer"
+    library: "Filer",
+    libraryTarget: "umd"
   },
   plugins: [
 		new NodePolyfillPlugin()
